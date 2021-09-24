dddd6e041bad2328038c8eb3069810ded494ac17
Nikola Karaklic <nikolakaraklic@gmail.com>
2021-09-24 13:19:30 +0200

    Add MUI production prefix for CSS (#17)

--- app2/container/src/App.js
+++ app2/container/src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+	StylesProvider,
+	createGenerateClassName,
+} from "@material-ui/core/styles";
 
-// Adding comment to trigger GH workflow
+const generateClassName = createGenerateClassName({
+	productionPrefix: "co",
+});
+
 function App() {
 	return (
-		<BrowserRouter>
-			<div>
-				<Header />
-				<MarketingApp />
-			</div>
-		</BrowserRouter>
+		<StylesProvider generateClassName={generateClassName}>
+			<BrowserRouter>
+				<div>
+					<Header />
+					<MarketingApp />
+				</div>
+			</BrowserRouter>
+		</StylesProvider>
 	);
 }
 
