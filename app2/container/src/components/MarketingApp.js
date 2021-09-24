c4a0bad9f8ce6a470588006ee20d1400861487ac
Nikola Karaklic <nikolakaraklic@gmail.com>
2021-09-24 22:54:49 +0200

    Multi tier navigation (#18)
    * Add multi-tier navigation for Container and Marketing
    
    * fix: Allow Marketing in isolation to refresh successfully on a predefined route

--- app2/container/src/components/MarketingApp.js
+++ app2/container/src/components/MarketingApp.js
@@ -1,11 +1,25 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 function MarketingApp() {
 	const ref = useRef(null);
+	// Browser History
+	const history = useHistory();
 
 	useEffect(() => {
-		mount(ref.current);
+		const { onParentNavigate } = mount(ref.current, {
+			onNavigate: ({ pathname: nextPathname }) => {
+				const { pathname } = history.location;
+
+				// Prevent infinite loops between history objects
+				if (pathname !== nextPathname) {
+					history.push(nextPathname);
+				}
+			},
+		});
+
+		history.listen(onParentNavigate);
 	}, []);
 
 	return <div ref={ref}></div>;
